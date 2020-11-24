d9ba9a63fd879f0721282560386297d151db906d
Oukki <osiljamo@gmail.com>
2020-11-24 03:55:30 +0200

    Add base for calendar doors

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="container">
-    <img id="background" src="@/assets/background.jpg" alt="Kiltis" >
+    <img id="door1" class="door" v-bind:class="{hide: doors.door1}" src="@/assets/advent-calendar.jpg" alt="Kiltis" @click="openDoor($event)">
+    <img id="door2" class="door" v-bind:class="{hide: doors.door2}" src="@/assets/advent-calendar.jpg" alt="Kiltis" @click="openDoor($event)">
+    <img id="background" src="@/assets/background1.jpg" alt="Kiltis" >
   </div>
 </template>
 
 <script>
 export default {
-name: "Background.vue"
+  name: "Background.vue",
+  data () {
+    return {
+      doors: {
+        door1: false,
+        door2: false,
+      },
+    }
+  },
+  methods: {
+    openDoor(event) {
+      this.$set(this.doors, event.target.id, true)
+      console.log(event.target.id)
+    }
+  }
 }
 </script>
 
 <style scoped>
 #container {
+  position: relative;
   height: 100%;
   width: 100%;
   overflow-x: scroll;
@@ -21,16 +38,32 @@ name: "Background.vue"
 }
 #container::-webkit-scrollbar { display: none; }
 #background {
+  position: absolute;
   left: 0;
   top: 0;
   height: 100vh;
   width: auto;
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  -webkit-tap-highlight-color: transparent;
+  z-index: 0;
+}
+.door {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: auto;
+  z-index: 1;
+}
+#door1 {
+  transition: visibility 0.5s, opacity 0.5s ease-in;
+  clip-path: inset(48% 72% 33% 16%);
+}
+#door2 {
+  transition: visibility 0.5s, opacity 0.5s ease-in;
+  clip-path: inset(31% 47% 49% 41%);
+}
+
+.hide {
+  visibility: hidden;
+  opacity: 0;
 }
 </style>
