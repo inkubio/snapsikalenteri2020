fe4b6cd1be9af2f6295859b8bab89cfa5b8b0345
Oukki <osiljamo@gmail.com>
2020-12-01 04:22:32 +0200

    Add door 3

--- src/components/Doors/Door3.vue
+++ src/components/Doors/Door3.vue
@@ -1,14 +1,25 @@
 <template>
-  <iframe src="https://emil.fi/p/inku18luukku/"/>
+  <div id="container">
+    <iframe :src="url" frameborder="0" allow="encrypted-media;"></iframe>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Door3"
+  name: "Door3",
+  data() {
+    return {
+      url: "https://emil.fi/p/inku18luukku/",
+    }
+  }
 }
 </script>
 
 <style scoped>
+#container{
+  width: 100%;
+  height: 100%;
+}
 iframe {
   width: 95%;
   height: 97%;
