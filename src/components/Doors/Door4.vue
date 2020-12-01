c8201d6e0fae02a436134ace6827bbceb782a03e
Oukki <osiljamo@gmail.com>
2020-12-01 04:31:35 +0200

    Add door 4

--- src/components/Doors/Door4.vue
+++ src/components/Doors/Door4.vue
@@ -1,5 +1,30 @@
 <template>
-  <p>Luukku 4</p>
+  <div id="container">
+    <p>4.12. NVOTEKJ</p>
+    <p>
+      Kulttuurintäyteistä joulunodotusta kanssakiltalaiset! Neljännen vuoden opiskelijatoiminnanedistämiskulttuurijaos
+      lähenee kunnioitettavaa puolen vuoden ikää ja halusimmekin koostaa teille hienoimmat hetkemme.
+    </p>
+    <p>
+      30.6. NVOTEKJ näki ensi kertaa päivänvalon. Sittemmin päivänvalo on paennut nähtyään NVOTEKJ:n
+    </p>
+    <p>
+      21.8. NVOTEKJ:n laitumellelasku. Huolellisesti ja hyvissä ajoin hankitut univormut nähtiin ensimmäistä
+      kertaa ilahduttamassa ja kauhistuttamassa.
+    </p>
+    <p>
+      6.9. NVOTEKJ:n ensimmäinen edustustehtävä, juhlapuhe killan puolivuotisvuosijuhlilla.
+      Kaikki todisteet tästä verbaalisesta mestariteoksesta on tuhottu kansallisen turvallisuuden vuoksi.
+    </p>
+    <p>30.9. NVOTEKJ kasvatti aatteensa seuraajakuntaa liittymällä sosiaaliseen mediaan.
+      Tästä lähtien seuraajien kesken ollaan arvottu huikeita voittoja.
+      Kukaan ei ole vielä voittanut. Silti vähemmän rigged kuin biopepu.
+    </p>
+    <p>
+      Mutta mitä NVOTEKJ tekee seuraavaksi? Jäämme seuraamaan tilannetta. Takaisin Pasilaan.
+    </p>
+    <a href="https://www.youtube.com/watch?v=131mx6Dmx4Y&feature=youtu.be" target="_blank">LINKK VIDEOON</a>
+  </div>
 </template>
 
 <script>
@@ -9,5 +34,13 @@ name: "Door4"
 </script>
 
 <style scoped>
-
+p {
+  font-size: 1.2em;
+}
+#container {
+  display: flex;
+  flex-flow: column;
+  height: 90%;
+  justify-content: space-between;
+}
 </style>
