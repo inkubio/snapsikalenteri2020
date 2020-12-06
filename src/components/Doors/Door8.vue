f59ec1cd078c282a046bfe18dbffc9a985d0b191
Oukki <osiljamo@gmail.com>
2020-12-06 18:47:23 +0200

    Add door 8

--- src/components/Doors/Door8.vue
+++ src/components/Doors/Door8.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>Luukku 8</p>
+  <div id="container">
+    <h3>Jouluresepti – Fudge</h3>
+    <video controls>
+      <source src="@/assets/data/video/Fudge2.mp4" type="video/mp4">
+      Selaimesi ei tue video tagia
+    </video>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,19 @@ export default {
 </script>
 
 <style scoped>
+#container {
+  width: 100%;
+  height: 100%;
+}
+h3 {
+  font-size: 1.4em;
+}
+p {
+  font-size: 1.2em;
+}
+video {
+  height: calc(100% - 2.3em);
+}
+
 
 </style>
