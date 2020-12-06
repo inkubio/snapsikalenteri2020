dc37337131fc77a46529ddad0717dc73a6d60af9
Oukki <osiljamo@gmail.com>
2020-12-06 18:09:00 +0200

    Add door 7

--- src/components/Doors/Door7.vue
+++ src/components/Doors/Door7.vue
@@ -1,5 +1,31 @@
 <template>
-  <p>Luukku 7</p>
+  <div id="container">
+    <h3>Joulusoittolista</h3>
+    <p>
+      Musiikki, tuo ihana elämänilon lähde… Vaikka kiltamme defacto syyslista onkin Last Christmas variaatiot et al,
+      voi vaihtelu joskus virkistää. Tiede on vihdoin löytänyt täydellisen joululaulun, joka yhdistää kaikki oleelliset
+      elementit riemukkaaksi rallatukseksi:
+      <a href="https://www.fi.edu/science-just-created-the-ultimate-christmas-song">Ultimaattinen joululaulu</a><br>
+      Valitettavasti kyseistä biisiä ei kuitenkaan löydy spotifysta, joten tilanne oli jotenkin korjattava. S’napsin toimittajat ottivat ohjat omiin käsiinsä.
+    </p>
+    <p>
+      Tässä onkin inkubiittien oma joululaululista! Listalle on kuratoitu 20 erilaista ja -makuista kipaletta
+      jouduttamaan joulunodotusta. Tarkoituksena on, että lista olisi kiltamme jäsenten näköinen, joten toivomme
+      ihmisten lisäävän itselleen tärkeitä joululauluja jatkamaan listaa. Vältetään kuitenkin kokonaisten albumien
+      spämmimistä, jotta jokaisella biisillä on mahdollisuus tulla kuulluksi. 🌲🎅
+    </p>
+    <p>P.s. kannattaa tutustua myös listan biisien albumeihin fiiliksen mukaan!</p>
+    <p>P.p.s Sisältövaroitus: Mikäli osallistut Whamageddoniin, suosittelemme varovaisuutta.</p>
+    <iframe
+        id="playlist"
+        src="https://open.spotify.com/embed/playlist/4tPPXi84WRREjzHjoxsPHo"
+        width="300"
+        height="380"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media">
+    </iframe>
+  </div>
 </template>
 
 <script>
@@ -9,5 +35,18 @@ name: "Door7"
 </script>
 
 <style scoped>
-
+h3 {
+  font-size: 1.4em;
+}
+p {
+  font-size: 1.2em;
+  margin: 0.5em 0;
+}
+#container {
+  height: 100%;
+}
+#playlist {
+  height: 100%;
+  width: 100%;
+}
 </style>
