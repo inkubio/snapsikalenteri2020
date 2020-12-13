bc106997430a92f450e5a2688c7087a1f7275594
Oukki <osiljamo@gmail.com>
2020-12-13 18:11:56 +0200

    Add door15

--- src/components/Doors/Door15.vue
+++ src/components/Doors/Door15.vue
@@ -1,5 +1,13 @@
 <template>
-  <p>Luukku 15</p>
+  <div id="container">
+    <h3>Last Christmas Nokkahuiluversio</h3>
+    <iframe
+        src="https://www.youtube-nocookie.com/embed/Wr1HySeEIe4"
+        frameborder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+    </iframe>
+  </div>
 </template>
 
 <script>
@@ -9,5 +17,17 @@ name: "Door15"
 </script>
 
 <style scoped>
-
+#container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+}
+h3 {
+  font-size: 1.4em;
+}
+iframe {
+  height: calc(100% - 3.2em);
+  width: auto;
+}
 </style>
