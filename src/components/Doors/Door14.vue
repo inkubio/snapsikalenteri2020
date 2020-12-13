e0c1b7163ab7553baaf89d1a6ee51061883f2880
Oukki <osiljamo@gmail.com>
2020-12-13 17:49:04 +0200

    Add door14

--- src/components/Doors/Door14.vue
+++ src/components/Doors/Door14.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>Luukku 14</p>
+  <div id="container">
+    <h3>Sʸnapsin glögiresepti</h3>
+    <a href="snapsi_jouluglogi_etiketti.pdf">Tulosta tästä etiketti</a>
+    <video controls poster="@/assets/data/door14/snapsin_jouluglogi.png">
+      <source src="@/assets/data/video/snapsi_jouluglogi.mp4" type="video/mp4">
+      Selaimesi ei tue html videoita.
+    </video>
+  </div>
 </template>
 
 <script>
@@ -9,5 +16,20 @@ name: "Door14"
 </script>
 
 <style scoped>
+#container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+}
+h3 {
+  font-size: 1.4em;
+}
+video {
+  height: calc(100% - 3.2em);
+}
+a {
+  text-underline: none;
+}
 
 </style>
