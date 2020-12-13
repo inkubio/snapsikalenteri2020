0df3d70bc21d419e737d57d48c83eadbd9d7549a
Oukki <osiljamo@gmail.com>
2020-12-13 19:33:19 +0200

    Add door 16

--- src/components/Doors/Door16.vue
+++ src/components/Doors/Door16.vue
@@ -1,5 +1,127 @@
 <template>
-  <p>Luukku 16</p>
+  <div id="container">
+    <h3>Kuinka paljon voi syödä piparitaikinaa, ennen kuin tulee paha olo?</h3>
+    <h4>Abstract</h4>
+    <p>Testasimme, kuinka paljon yhtäjaksoiseti pitää syödä piparitaikinaa ennenkuin tulee paha olo.
+      Ensiksi määrittelimme pahalle ololle numeroasteikon. Koehenkilöinä toimivat Antti Huttunen ja Susan Rämä.
+    </p>
+    <h5>Pahanolon asteikko</h5>
+    <ol>
+      <li>ei tunnu missään</li>
+      <li>semi ok vielä</li>
+      <li>nyt riittää</li>
+      <li>paha olo</li>
+      <li>se on oksettavaa</li>
+    </ol>
+    <p>
+      Taikinana käytimme MyllynParas Laktoositon Piparitaikinaa, joka on myös piparinleivonnan Suomen mestari kilpailujen virallinen kisataikina.
+    </p>
+    <div id="pics1">
+      <img src="@/assets/data/door16/kuva1.jpg">
+      <img src="@/assets/data/door16/kuva2.jpg">
+    </div>
+    <p>
+      Susan lämmitteli edellisenä päivänä syömällä taikinaa ja juomalla punaviiniä.
+      Molemmat syöneet puolitoista tuntia sitten. Susan söi mandariinin ennen koetta.
+      Syödään viiden minuutin välein 50 grammaa taikinaa. Taikinan kyytihenkilönä glögiä. 500 grammaa taikinaa vastaa 1960 kcal.
+    </p>
+    <p>
+      Mittasimme kulutettua aikaa myös Last Christmaseilla. Mittarimme kuitenkin sekosi pahan olon kohdalla, joten lukumäärä on suuntaa antava.
+      Last Christmas: 11 skipattu: 2
+    </p>
+    <h4>Results</h4>
+    <p>Koe alkoi 13:40 Suomen aikaa 3.12.2020 osoitteessa JMT 10, 02150 Espoo.</p>
+    <p>
+      <b>13:40 - 50 g</b><br>
+      Susan: 1 <br>
+      Antti: 1 <br>
+      Taikinan koostumus ei ole ehkä paras: se jää vähän kitalakeen kiinni.
+    </p>
+    <p>
+      <b>13:45 - 100 g</b><br>
+      Susan: 1 <br>
+      Antti: 1 <br>
+      Taikina vielä kivan viileää ja sopivan mausteista. Glögit valmistui.
+    </p>
+    <p>
+      <b>13:50 - 150 g</b><br>
+      Susan: 1 <br>
+      Antti: 2 <br>
+      paketissa mainitaan paperitötteröpursotin
+    </p>
+    <p>
+      <b>13:55 - 200 g</b><br>
+      Susan: 2 <br>
+      Antti: 2 <br>
+      Rasva ja sokeri alkavat maistua erottuvasti ja jauho jää kitalakeen kiinni.
+      Toinen puoli taikinasta on jauhoisempaa kuin toinen. <br>
+      13:58 Susan: Nyt hyvä fiilis, vois lopettaa.
+      Veikkaus, että päästään 4:seen
+    </p>
+    <p>
+      <b>14:00 -250 g</b><br>
+      Susan: 3 <br>
+      Antti: 2,8 <br>
+      Taikinan syönti hitaampaa: alkuun taikinapala meni ykkösellä alas nyt pala pitää pureskella hitaasti.
+    </p>
+    <p>
+      <b>14:05 -300g</b><br>
+      Susan: 3,1 <br>
+      Antti: 3,3 <br>
+      Kumpikaan ei innoissaan enää taikinapalasta. Antti aloitti leipomaan. Mitä, jos ei saa syötyä 5 min palasta?
+    </p>
+    <img class="pic" src="@/assets/data/door16/kuva3.jpg">
+    <p>
+      <b>14:10 - 350 g</b><br>
+      Susan: 4,5 <br>
+      Antti: 3,9 <br>
+      Susan pilkkoi palasen pienemmiksi. “Hyi Vittu! Mä en syö enää ikinä piparitaikinaa. Maha tuntuu jo täydeltä” <br>
+      14:11 Antti: 4,1 <br>
+      Mausteet tulee voimakkaasti esiin ja taikina maistuu karvaalle ja happamalle
+      Lasi punaviiniä vastaa noin tunnin kuntoilua, joten taikinan kulutukseen tarvitaan pullo viiniä.
+    </p>
+    <img class="pic" src="@/assets/data/door16/kuva4.jpg">
+    <p>
+      <b>14:15 tauko</b><br>
+      Susan: 4,8 <br>
+      Antti: 4,3 <br>
+      Susan: “Mä en välttämättä kykene enää” <br>
+      Antti: “vähän kuin suklaasitsit.”
+    </p>
+    <p>
+      <b>14:20 - 375g</b><br>
+      Susan: 5 <br>
+      Antti: 4,7 <br>
+      Kumpi mieluummin Korona vai taikinoiden loppuun syönti? <br>
+      Susan: “Mieluummin korona. Tää on tosi pahaa. 10 grammaan menee 2,5 min <br>
+      Antti: “Taikina” <br>
+      Susan ei halunnut olla luuseri ja otti kumminkin lisää. “Teki vähän mieli vielä”
+      Antin kommentit 14:28  “jos ton ois syöny loppuu ni ois tullu yrjis. oon iha varma siitä”
+    </p>
+    <p>
+      <b>14:30</b><br>
+      Susan: 5 <br>
+      Antti: 4 <br>
+      Susanilla taikina potkii takaisin päin ja päätäkin alkaa vähän särkeä. <br>
+      Antilla solid 4 kun ei ole tarvinnut enää syödä taikinaa.
+    </p>
+    <p>
+      <b>14:40</b><br>
+      Susan: 4 <br>
+      Olo helpottaa hieman. Susania ällöttää ajatus edes nähdä piparitaikinaa. <br>
+      14:43 Antti otti palan lisää.
+    </p>
+    <img class="graph" src="@/assets/data/door16/kuva5.png">
+    <h4>Conclusion and discussion</h4>
+    <p>
+      Piparitaikinaa pystyy syömään pidemmällä aikavälillä huomattavasti enemmän, nimimerkillä kokemusta on,
+      mutta kohtasimme äärirajan 50g/5min vauhdilla noin 350g kohdalla. Ennen koetta pohdimme mahdollista tilannetta,
+      että mikäli paha olo ei iskisi vielä 350g kohdalla tulisi lähteä kohti A-Blocia ostamaan lisää, mutta tälle
+      ei kuitenkaan tullut tarvetta. Olo puoli tuntia syömisen lopettamisen jälkeen on jo ihan ok.
+      “Ei enää piparitaikinaa tänään” t. Susan <br>
+      “En suosittele ketään syömään näin paljoa taikinaa” t. Antti
+    </p>
+  </div>
 </template>
 
 <script>
@@ -9,5 +131,40 @@ export default {
 </script>
 
 <style scoped>
+#container {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+h3 {
+  font-size: 2em;
+}
+h4 {
+  font-size: 1.5em;
+}
+h5 {
+  text-align: start;
+  font-size: 1.3em;
+}
+p {
+  font-size: 1.2em;
+  margin: 0.5em auto;
+}
+ol {
+  font-size: 1.2em;
+  text-align: start;
+}
+#pics1 > img {
+  width: 50%;
+  max-width: 300px;
+}
+.pic {
+  width: 100%;
+  max-width: 300px;
+}
+.graph {
+  width: 100%;
+  max-width: 550px;
+}
 
 </style>
