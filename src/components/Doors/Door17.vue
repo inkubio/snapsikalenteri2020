3ad82bd8d53ecfb44623aa316ab673d4e1e0db29
Oukki <osiljamo@gmail.com>
2020-12-16 20:21:34 +0200

    Add door 17

--- src/components/Doors/Door17.vue
+++ src/components/Doors/Door17.vue
@@ -1,13 +1,142 @@
 <template>
-  <p>Luukku 17</p>
+  <div id="container">
+    <h3>Pipariohje</h3>
+    <h4>Kuinka leipoa tarpeeksi pipareita</h4>
+    <p>
+      On 17. joulukuuta. Kaikki itsenäisyyspäiväviikonloppuna leipomasi piparit on jo loppuneet, sillä et leiponut
+      niitä tarpeeksi. Eikä ole edes jouluaatto vielä! Ja mikä pahinta, eilisessä luukussa syötiin kaikki kaupan
+      piparitaikina. Ei hätää hyvä ystävä, nyt on mahdollisuus olla etukäteen viisaampi, jotta ensi vuonna itse leivotut
+      piparkakut riittävät ainakin Tapaninpäivään asti. Kuten ehkä voit kuvitella, ratkaisu pulmaan on yksinkertainen:
+      Leivo tarpeeksi pipareita. Nyt päätoimittaja Vilma kertoo miten se tehdään.
+    </p>
+    <p>
+      Todistetusti ainakin vuodesta 2007 olemme joka marras-joulukuun taitteessa käyneet mummin luona leipomassa
+      kerralla kunnolla pipareita. Näin niitä riittää ihanasti naposteltavaksi koko joulukuuksi. En nyt paljasta vielä
+      muuta piparien lopullisesta määrästä, kuin että piparien määrää ei ole pitkään aikaan meillä laskettu pipareissa,
+      vaan pellillisissä. Tässä käyttämämme ohje ja tämän vuoden saldo, sekä muutama vinkki.
+    </p>
+    <img class="pic1" src="@/assets/data/door17/kuva1.jpg" />
+    <h4>Tarvikkeet</h4>
+    <h5>Välttämättömät</h5>
+    <ul>
+      <li>Uuni</li>
+      <li>Ainekset taikinaan</li>
+      <li>Piparimuotti</li>
+      <li>Kaulin</li>
+    </ul>
+    <h5>Hyödylliset</h5>
+    <ul>
+      <li>Kiertoilmauuni, jotta saa paistettua monta pellillistä kerrallaan</li>
+      <li>Paljon piparimuotteja, ettei tule tylsää</li>
+      <li>Joku joka tekee taikinan puolestasi edellisenä päivänä (lue: äiti)</li>
+      <li>Joku joka osaa kaulita taikinan ilman että se jää kiinni pöytään (lue: äiti)</li>
+      <li>Apureita (katso kuva)</li>
+    </ul>
+    <img class="pic2" src="@/assets/data/door17/kuva2.jpg" />
+    <h4>Taikina & Leipominen</h4>
+    <p>
+      Ollaan käytetty Päivi Laineen Kodin kuvalehden voitto-ohjetta numerosta 18/1996. Tärkein huomio on, että alla
+      oleva taikinaohje on yksinkertainen, ja jotta saat tarpeeksi pipareita, pitää sitä tehdä enemmän.
+      Historiankirjojen mukaan vuosina 2008-2018 teimme nelinkertaisen taikinan, mutta viime vuonna ja tänä vuonna
+      teimme viisinkertaisen taikinan. Mikä sen parempi tapa aloittaa joulukuu, kuin kilo voita ja kilo sokeria!
+      Kolminkertaisen taikinan saa vielä valmistettua hyvin isoon kattilaan, mutta nelin- tai viisinkertainen taikina
+      kannattaa jakaa kahtia jo valmistusvaiheessa. Sitä nimittäin on aika paljon.
+    </p>
+    <ul>
+      <li>250 g voita</li>
+      <li>200 g (reilu 2 dl) sokeria</li>
+      <li>1 ½ dl siirappia</li>
+      <li>2 tl neilikkaa</li>
+      <li>2 tl kanelia</li>
+      <li>1 ½ tl inkivääriä</li>
+      <li>1 tl pomeranssinkuorta</li>
+      <li>¾ dl kaakaojuomajauhetta</li>
+      <li>2 munaa</li>
+      <li>3 tl soodaa</li>
+      <li>1 tl suolaa</li>
+      <li>noin 500 g (noin 8 dl) vehnäjauhoja</li>
+      <li>Pinnalle: kidesokeria</li>
+    </ul>
+    <ul>
+      <li>Mittaa kattilaan rasva, sokeri, siirappi, mausteet ja kaakaojuomajauhe. Kiehauta, anna jäähtyä ja vatkaa
+        vaahdoksi. Lisää mukaan munat yksitellen reippaasti vatkaten.
+      </li>
+      <li>Sekoita sooda ja suola jauhoihin. Lisää jauhot taikinaan varovasti sekoitellen. Anna taikinan rauhoittua
+        jääkaapissa yön yli.
+      </li>
+      <li>Kauli taikina ohueksi. Ripottele taikinalevylle tasainen ohut kerros kidesokeria kaulimisen loppuvaiheessa.
+        Ota muoteilla levystä piparkakkuja, nosta leivinpaperilla päällystetylle pellille ja paista 180 asteessa noin 6
+        minuuttia.
+      </li>
+    </ul>
+    <div>
+      <img class="pic3" src="@/assets/data/door17/kuva3.jpg" />
+      <img class="pic3" src="@/assets/data/door17/kuva4.jpg" />
+    </div>
+    <h4>Laskenta</h4>
+    <p>
+      Oikeastaan laskenta (eli peltien laskeminen) pitää aloittaa jo kun ensimmäiset pellilliset pipareita menevät
+      uuniin. Kannattaa hyödyntää joko “sisään menneet” tai “ulos tulleet” menetelmää, eli laskea pellit joko kun ne
+      menevät uuniin, tai kun ne tulevat uunista. Muuten riski on että jokin pelti lasketaan kahdesti. Me tehtiin tänä
+      vuonna aika monta pellillistä, 75 jos tarkkoja ollaan. Kuvassa on noin 60% kaikista leivotuista pipareista, sillä
+      äiti jatkoi vielä sen jälkeen kun me oltiin lähdetty kotiin, kiitti äiti!
+    </p>
+    <img class="pic4" src="@/assets/data/door17/kuva5.jpg" />
+    <div>
+      <img class="pic3" src="@/assets/data/door17/kuva7.jpg" />
+      <img class="pic3" src="@/assets/data/door17/kuva6.jpg" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Door17"
+  name: "Door17"
 }
 </script>
 
 <style scoped>
-
+#container {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+h3 {
+  font-size: 2em;
+  margin-bottom: 0.75em;
+}
+h4 {
+  font-size: 1.5em;
+}
+h5 {
+  text-align: start;
+  font-size: 1.3em;
+}
+p {
+  font-size: 1.2em;
+  margin: 0.5em auto;
+  text-align: start;
+}
+ul {
+  font-size: 1.2em;
+  text-align: start;
+  list-style: none;
+  padding: 0;
+  margin: 1em 0;
+}
+.pic1 {
+  width: 450px;
+  max-width: 100%;
+}
+.pic2 {
+  width: 450px;
+  max-width: 100%;
+}
+.pic3 {
+  margin: 1%;
+  max-width: 48%;
+}
+.pic4 {
+  max-width: 98%;
+}
 </style>
