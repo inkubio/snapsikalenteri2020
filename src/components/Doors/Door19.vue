72415b5bc528ce0e314681120153c6d26575add7
Oukki <osiljamo@gmail.com>
2020-12-16 18:18:34 +0200

    Add door 19

--- src/components/Doors/Door19.vue
+++ src/components/Doors/Door19.vue
@@ -1,5 +1,136 @@
 <template>
-  <p>Luukku 19</p>
+  <div>
+    <h3>Poroskooppi</h3>
+    <p>
+      Joulu on taas, joulu on taas, S’napsi täynnä poroskooppeja. Poroskoopista näet, kuka joulupukin poroista on
+      sinun toteemiporosi ja mitä ihanaa sinulle tulee tapahtumaan joululomalla.
+    </p>
+    <div id="poroscopes">
+      <div class="poroscope">
+        <img src="@/assets/data/door19/1.png"/>
+        <h4>Poku  20.1.-19.2.</h4>
+        <p>
+          Vietät rauhallista joulua vanhempiesi luona, ja kaipaat muutaman päivän jälkeen kipeästi tekemistä.
+          Vanhempasi vain kyselevät sinulta, milloin aiot valmistua ja miksi et ole soittanut heille taas kuukausiin.
+          Lähdet joulupäivänä heitä pakoon lapsuuden kotikaupunkisi ainoaan baariin,
+          ja törmäätkin siellä yläastevuosien ihastukseesi.
+          Tällä kertaa, toisin kun silloin muutama vuosi sitten kun olit nolo teini, välillänne on molemminpuolisia
+          rakkauden signaaleja.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/2.png"/>
+        <h4>Ailu 20.2.-20.3.</h4>
+        <p>
+          Koittaa odottamasi jouluaamu ja näet taivaalla loistavan kirkkaan tähden, jota lähdet seuraamaan.
+          Saavut Suomen enkelten kaupunkiin Mikkeliin, muttet löydä mistään majapaikkaa, jonne jäädä yöksi.
+          Sinut päästetään kuitenkin bar Päämajaan illalla ja menet katsomaan joulun ihmettä: Maria Monden soolouran
+          comeback-keikkaa.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/3.png"/>
+        <h4>Suivakka 21.3.-19.4.</h4>
+        <p>
+          Sukulaisesi ja sinä olette saaneet rankasta vuodesta tarpeeksenne, ja nyt on vihdoin aika nollata.
+          Joulupöytä notkuu herkuista, joulukuusi on koristeltu ja Last Christmas -soittolista soi loopilla.
+          On aika hiljentyä joulun viettoon...ja vetää kunnon kännit perheen kesken.
+          Soihtujen lisäksi myös sinä sammut jo pääruoan jälkeen.
+          Heräät joulupäivään pöydän alta, mihin äitisi on ystävällisesti tuonut sinulle kaikki joululahjasi sekä pari
+          tasoittavaa.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/4.png"/>
+        <h4>Mutsikki 20.4.-20.5.</h4>
+        <p>
+          Pääset vihdoin rankan opiskelusyksyn jälkeen koti-kotiin katsomaan vanhempiasi ja muuta perhettäsi.
+          Joulusi on piparintuoksuinen ja lämpimän glögin täytteinen.
+          Löydät joulupuurosta mantelin ja saat kaikki toivomasi lahjat.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/5.png"/>
+        <h4>Valkko 21.5.-20.6.</h4>
+        <p>
+          Rakennat koti-kotisi takapihalle maailman kauneimman ja suurimman lumiukon.
+          Seuraavana yönä et saa unta vaan käyt katsomassa kyhäämääsi lumiukkoa, joka herääkin yhtäkkiä henkiin.
+          Lumiukko tarttuu sinua kädestä ja lennättää sinut katsomaan lumiukkojen joulua.
+          Siellä tapaat joulupukin ja saat häneltä sinisen kaulahuivin lahjaksi.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/6.png"/>
+        <h4>Tilkku 21.6.-22.7.</h4>
+        <p>
+          Tänä jouluna sinä saat pukilta. Ja nyt ei puhuta lahjoista. Olet ollut tänä vuonna niin tuhma,
+          että pukin täytyy hieman rangaista. Prujasit kaikki laskarit, käytit Kelan rahet viinaan ja kuksit fuksia
+          heti orientaatioviikolla. Hyi sinua! Voisit parantaa tapojasi ensi vuonna, mutta toisaalta taisit
+          kuitenkin tykätä ja teet samoin myös jatkossa.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/7.png"/>
+        <h4>Sipsu 23.7.-23.8.</h4>
+        <p>
+          Joulu koittaa jälleen ja voit todeta, että joudut taas menemään koti-kotiin. Joudut ostamaan ylihintaisen
+          junalipun ja heti kotiin päästyäsi alkaa massiivinen jouluriita.
+          Vanhempasi eivät ole töiltään kerenneet yhtään valmistelemaan joulua ja heitä stressaa ihan hirveästi.
+          Onneksi voit vetäytyä koko jouluksi omaan huoneeseesi pelaamaan Fortnitea ja Among Us:a.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/8.png"/>
+        <h4>Täpy 24.8.-23.9.</h4>
+        <p>
+          Tänä jouluna on vihdoin sinun vuorosi loistaa! Nimittäin murskaat vanhempasi perheesi perinteisessä
+          jokavuotisessa kellotuskilpailussa elämäsi ensimmäisen kerran! Tämä on se hetki jota varten olet treenannut
+          koko opiskeluaikasi. Nyt voitkin tulevana vuotena keskittyä toissijaisiin tavoitteisiisi, kuten opintojen
+          edistämiseen tai elämäntapojen parantamiseen.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/9.png"/>
+        <h4>Turpo 23.9.-23.10.</h4>
+        <p>
+          Joulupukilla on jouluna miljardeja lahjoja vietävänään. Ei ole siis mikään ihme, jos muutaman talon
+          lahjat jäävät vahingossa viemättä kaikessa hässäkässä. Tänä vuonna sinun talosi on yksi niistä.
+          Joulupukki unohtaa käydä sinun perheesi luona ja jäättekin kaikki täysin ilman lahjoja.
+          Toivottavasti ensi vuonna on parempi tuuri.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/10.png"/>
+        <h4>Pyry 24.10.-22.11.</h4>
+        <p>
+          Pimeä ja raskas vuosi saa yllättävän käänteen, kun elämääsi saapuu uusi henkilö. Hän saa joulusukat pyörimään
+          jaloissasi ja et saa mitään aikaiseksi, kun ajattelet vain häntä. Unohdat niin joululahjojen ostamisen kuin
+          tentteihin valmistautumisen. Feidaat sukujoulun viime hetkellä, jotta voisit viettää myös joulunpyhät hänen
+          seurassaan.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/11.png"/>
+        <h4>Kipinä 23.11.-21.12.</h4>
+        <p>
+          Pääset vihdoinkin koti-kotiin ja tiedät, että voit unohtaa kaikki koulukiireet. Voit vetää päällesi kaikista
+          rumimman jouluvillapaitasi, leipoa ihania joululeivonnaisia ja koristella talo joulukuntoon.
+          Kyynisestä väsyneestä teekkarista kuoriutuu suorastaan joulun henkilö (joulun hengettären neutraali muoto).
+          Tuot näin iloa ja onnea koko perheellesi.
+        </p>
+      </div>
+      <div class="poroscope">
+        <img src="@/assets/data/door19/12.png"/>
+        <h4>Petteri 22.12.- 19.1.</h4>
+        <p>
+          Olet ollut tänä vuonna erittäin kiltti ja vanhem….joulupukkikin on huomannut tämän.
+          Saatkin lahjaksi omistusasunnon. Ainoa haittapuoli on, että asunto sijaitsee Enontekiöllä, etkä ole
+          valmis muuttamaan sinne. Kuitenkin suunnittelet kaveriesi kanssa Lapin matkan uuteen omistusasuntoosi ja
+          päätät vuokrata asunnon matkan jälkeen.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +140,29 @@ name: "Door19"
 </script>
 
 <style scoped>
-
+#container {
+  width: 100%;
+}
+h3 {
+  font-size: 1.4em;
+}
+p {
+  font-size: 1.2em;
+}
+#poroscopes {
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  flex-wrap: wrap;
+}
+.poroscope {
+  margin: 1em 0;
+  width: 100%;
+  max-width: 600px;
+}
+img {
+  max-height: 350px;
+  max-width: 100%;
+}
 </style>
