ce05c78dcc5528a6221768e08e74c2189ce6190a
Oukki <osiljamo@gmail.com>
2020-12-16 17:54:01 +0200

    Add door 18

--- src/components/Doors/Door18.vue
+++ src/components/Doors/Door18.vue
@@ -1,5 +1,56 @@
 <template>
-  <p>Luukku 18</p>
+  <div id="container">
+    <h3>Ulkomaan kirjeenvaihtaja: terveiset Vantaalta</h3>
+    <p>
+      Olen kohdannut vuoden mittaan vitsailua, kuinka ‘hyvin’ ulkomaankirjeenvaihtajana minulla S’napsissa on mennyt.
+      Nyt on joulukuu, ja päätin viimein toteuttaa ensimmäisen jutun ulkomaankirjeenvaihtajana.
+      Osoitan, etten turhaan ollut tässä roolissa, vaikka nyt saatavilla ei olekaan hienoja ja aurinkoisia kuvia
+      Aasian lämmöstä. Sen sijaan matkustin pitkän matkaa Espoon sivistyksestä Vantaalle asti tätä varten tekemään
+      haastattelua koko elämänsä Vantaalla viettäneestä. Kokemus oli varsin erikoinen, koska tuntui ettei kaikesta
+      aina saanut täysin selvää. Olin kuitenkin varsin otettu tästä erityisestä mahdollisuudesta.
+      Nyt pääsette kuulemaan millaista elämä kaukana Vantaalla on näinä pimeinä aikoina:
+    </p>
+    <h4>“Mitäs tällä hetkellä kuuluu ihan yleisesti? Miten päivät ovat menneet?”</h4>
+    <p class="answer">
+      No mitäs tässä, melko normaalia elämää olen voinut elää. Paljon on tullut ulkoiltua, mutta välillä on toisaalta
+      ollut pimeinä aikoina vain olla sisällä lämpimässä. Glögiäkin olen jo hieman ennättänyt maistaa.
+      Ehkä jopa nykyisin nautin aika paljon tällaisesta kotoilusta ja siitä, ettei tarvitse kiireellä lähteä
+      aina jonnekin.
+    </p>
+    <h4>“Puhuit ulkoilevasi paljon. Onko täällä millaista liikkua ulkona?”</h4>
+    <p class="answer">
+      Kyllä sitä usein useampaan kertaan päivässä tulee käytyä ulkona, mutta välillä voin myöntää jääväni varsin
+      omissa tiloissani jonnekin noihin puskiin, välillä ihan keskellä päivää.
+      Muut toki ovat varoittaneet huumepiikeistä siellä pensaiden seassa, mutta en oikeen ole jaksanut kuunnella. <br>
+      Mitä tulee tähän uuteen tilanteeseen, koen että täällä suunnilla on muutoinkin hyvä pitää välimatkaa kaikkiin ihmisiin. Olen varsin epäileväinen muita kohtaan, koska sanoisin ettei koskaan voi tietää ketä vastaan tulee.
+    </p>
+    <h4>“Ovatko kaikki tuon kaltaisia ja onko tuo normaalia täällä?”</h4>
+    <p class="answer">
+      No mun perhe on kyllä varsin mukavaa, mutta kuten mainittu pidän tätä epäilyttävyyttä täällä täysin normaalina.
+      Puolestaan Espoossa minulla on kyllä muutamia oikein ihania ystäviä, joten uskon että siellä saattaisi olla
+      varsin mukavampaa porukkaa tämän empiirisen tutkimuksen kautta.
+    </p>
+    <h4>“Etkö ole harkinnut muuttoa jonnekin muualle, kuten sitten vaikka Espooseen?”</h4>
+    <p class="answer">
+      Voi kun voisinkin siihen vaikuttaa. Harmillisesti perheessäni on vähän harmillinen ja epätasa-arvoinen tilanne,
+      joten ei ole minun päätännässäni missä asumme yhdessä. Toisaalta en ole se henkilö, joka siivoaa tai tekee ruokaa,
+      mutta olisi silti mukavaa enempi vaikuttaa omiin asioihin.
+    </p>
+    <h4>“Mitä odotat joululta? Oletko jo tehnyt jouluvalmisteluita?”</h4>
+    <p class="answer">
+      Tottakai sitä ruokaa ja niitä kaikkia ihania tuoksuja ! Toivon saavani edes joitain näistä ihanista
+      jouluherkuista, vaikka olenkin jo tottunut etsimään ruokaa syötäväksi myös kaduilta.
+      Ennen joulua toivon pääseväni vielä kunnolla peseytymään, ja sitten voisin suunnata mökille rauhoittumaan joulun
+      pyhiksi.
+    </p>
+    <h4>“Kiitos kovasti haastattelusta ! Haluaisitko vielä päästä sanomaan jotain juttua lukeville?”</h4>
+    <p class="answer">Haluan toivottaa ihanaa joulun odotusta ihan kaikille ja haluisin, et nautit siitä mitä sulla just nyt on !</p>
+    <p>
+      Haastateltava oli valmistautunut varsin jouluisin somistuksin, joten oli varsin kunnia päättää tämä haastattelu
+      ottamalla kuva tästä vantaalaisesta:
+    </p>
+    <img src="@/assets/data/door18/image1.jpg" alt="Kuva haastateltavasta">
+  </div>
 </template>
 
 <script>
@@ -9,5 +60,32 @@ name: "Door18"
 </script>
 
 <style scoped>
+#container {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+h3 {
+  font-size: 1.4em;
+  margin-bottom: 1em;
+}
+p {
+  margin-bottom: 1em;
+  text-align: start;
+  font-size: 1.2em;
+}
+.answer  {
+
+}
+h4 {
+  margin-bottom: 1em;
+  text-align: start;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+img {
+  width: 100%;
+  max-width: 400px;
+}
 
 </style>
