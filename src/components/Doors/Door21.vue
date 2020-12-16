e67670bf001869085aaec9c94bfb5005764a7514
Oukki <osiljamo@gmail.com>
2020-12-16 18:27:24 +0200

    Add door 21

--- src/components/Doors/Door21.vue
+++ src/components/Doors/Door21.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>Luukku 21</p>
+  <div id="container">
+    <h3>Hali 2021 esittää:</h3>
+    <video controls>
+      <source src="@/assets/data/video/hali21.mp4" type="video/mp4">
+      Selaimesi ei tue html videoita.
+    </video>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,16 @@ export default {
 </script>
 
 <style scoped>
-
+#container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+}
+h3 {
+  font-size: 1.4em;
+}
+video {
+  height: calc(100% - 3.2em);
+}
 </style>
