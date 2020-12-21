31c4de01a461fddf4ddbe7318c8d1ab29ea0f6fb
Oukki <osiljamo@gmail.com>
2020-12-21 20:49:59 +0200

    Add door 22

--- src/components/Doors/Door22.vue
+++ src/components/Doors/Door22.vue
@@ -1,13 +1,30 @@
 <template>
-  <p>Luukku 22</p>
+  <div id="container">
+    <h3>Laita vaik S'napsi esittää: Tonttu</h3>
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/rK2PS9wvIbc" frameborder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Door22"
+  name: "Door22"
 }
 </script>
 
 <style scoped>
-
+#container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+}
+h3 {
+  font-size: 1.4em;
+}
+iframe {
+  height: calc(100% - 3.2em);
+  width: auto;
+}
 </style>
