d400f8d116a83fd26c8c867aed3835b76f3d2280
Oukki <osiljamo@gmail.com>
2020-12-21 18:51:34 +0200

    Add door 24

--- src/components/Doors/Door24.vue
+++ src/components/Doors/Door24.vue
@@ -1,13 +1,31 @@
 <template>
-  <p>Luukku 24</p>
+  <div id="container">
+    <h3>Hyvää joulua!</h3>
+    <p>
+      Haluamme toivottaa koko S’napsin puolesta oikein rauhallista ja turvallista joulua, sekä mahtavaa uutta vuotta
+      2021!
+    </p>
+    <p id="paatoimittaja">- Päätoimittajat 2020 Sofia & Vilma</p>
+    <img src="@/assets/data/door24/Joulukuva1.png" />
+  </div>
 </template>
-
 <script>
 export default {
-name: "Door24"
+  name: "Door24"
 }
 </script>
 
 <style scoped>
+h3 {
+  font-size: 1.4em;
+  margin-bottom: 1em;
+  text-align: center;
+}
 
+p {
+  font-size: 1.2em;
+  margin: 1em auto;
+  width: 100%;
+  max-width: 600px;
+}
 </style>
