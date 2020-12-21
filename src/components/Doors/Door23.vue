8f517e27734337ef4e0445e9bd2180f60033f050
Oukki <osiljamo@gmail.com>
2020-12-21 18:53:26 +0200

    Add door 23

--- src/components/Doors/Door23.vue
+++ src/components/Doors/Door23.vue
@@ -1,5 +1,25 @@
 <template>
-  <p>Luukku 23</p>
+  <div id="container">
+    <h3>3 in 1 joulurauhan julistus</h3>
+    <p>
+      Suomen Turku julistaa perinteisen joulurauhan huomenna jouluaattona tyhjälle Suurtorille. Oletko koskaan
+      ajatellut, että mitä jos julistuksen sanat menisivät vaikka sekaisin?
+    </p>
+    <h4>3 in 1 joulurauhan julistus aakkosjärjestyksessä</h4>
+    <ul>
+      <li>Järjestä takaisin alkuperäiseen järjestykseen</li>
+      <li>Lisää sopivat välimerkit ja luo uudenlainen tarina</li>
+      <li>Käytä bingona seuratessasi joulurauhanjulistusta kotisohvalta</li>
+    </ul>
+    <p>
+      armorikas asetukset asiaankuuluvalla asianhaarain asukkaille erikseen hartaudella Herramme hiljaisesti huomenna
+      häiritsee ja ja ja ja ja ja joka jollakin jonka jos joulujuhlaa joulujuhlaa joulurauha juhlaa julistetaan Jumala
+      kaikille kaikkia kaupungin kehoittamalla kustakin käyttäytymään käytöksellä laittomalla laki lopuksi meidän
+      muutoin on on rangaistukseen raskauttavien rauhallisesti rauhan riemullista rikkomuksesta rikkoo rikoksesta se
+      sekä siihen siis sillä sopimattomalla suo syntymäjuhla syypää säätävät taikka toivotetaan tämän täten tätä
+      vallitessa Vapahtajamme viettämään yleinen
+    </p>
+  </div>
 </template>
 
 <script>
@@ -9,5 +29,27 @@ export default {
 </script>
 
 <style scoped>
-
+#container {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  text-align: start;
+}
+h3 {
+  font-size: 1.4em;
+  margin-bottom: 1em;
+  text-align: center;
+}
+h4 {
+  font-size: 1.2em;
+  font-weight: normal;
+}
+ul {
+  font-size: 1.1em;
+  list-style: circle;
+}
+p {
+  font-size: 1.2em;
+  margin: 1em 0;
+}
 </style>
